c163273992eb975b849ae5eb4446be469b229c62
Dmitry Ryabikov <dmitryryabickov@yandex.ru>
2025-06-07 22:51:38 +0300

    Update brom.sv

--- rtl/brom.sv
+++ rtl/brom.sv
@@ -3,7 +3,7 @@ module brom #(
     parameter MEM_DEPTH   = 66,
     parameter MEM_FILE    = "",
     parameter ADDR_WIDTH  = $clog2(MEM_DEPTH)
-)(
+) (
     input  logic                  clk_i,
     input  logic [ADDR_WIDTH-1:0] addr_i,
     output logic [MEM_WIDTH-1:0]  data_o
