458ba250e5446cc013685932204fb00922015501
Dmitry Ryabikov <dmitryryabickov@yandex.ru>
2025-06-10 23:32:15 +0300

    Update brom.sv

--- rtl/brom.sv
+++ rtl/brom.sv
@@ -1,8 +1,8 @@
 /* verilator lint_off TIMESCALEMOD */
 module brom #(
-    parameter DATA_WIDTH = 16,
-    parameter ADDR_WIDTH = 32,
-    parameter MEM_FILE   = ""
+    parameter int DATA_WIDTH = 16,
+    parameter int ADDR_WIDTH = 32,
+    parameter     MEM_FILE   = ""
 ) (
     input  logic                  clk_i,
     input  logic [ADDR_WIDTH-1:0] addr_i,
